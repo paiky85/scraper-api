279effa4e056779bac568459b2bfc82e81fc050d
paiky85 <166179500+paiky85@users.noreply.github.com>
2024-10-28 19:58:39 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express, { response } from 'express';
 import cors from 'cors';
 import * as cheerio from 'cheerio';
 import unirest from 'unirest';
-import { selectRandom } from './userAgent.js';
+import { selectRandomUserAgent } from './userAgent.js';
 import utf8 from 'utf8';
 
 const app = express();
@@ -10,17 +10,18 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-// 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
+
 app.post('/', async (req, res) => {
   const keyword = req.body.keyword.trim().split(' ').join('+'); // Convert user input to search keyword
   const encodedURL = encodeURIComponent(keyword);
   const url = `https://www.google.com/search?gl=cz&hl=cs&q=${encodedURL}`;
+  console.log(url);
 
-  let userAgent = selectRandom(); // import from userAgent.js for selecting random user-agent
+  let userAgent = selectRandomUserAgent(); // import from userAgent.js for selecting random user-agent
   let headers = {
     'User-Agent': userAgent,
   };
-  console.log(headers);
+
   unirest
     .get(url)
     .headers(headers) //response.body
@@ -53,37 +54,3 @@ app.post('/', async (req, res) => {
 });
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
-
-/*
-  unirest
-    .get(url)
-    .headers(headers) //response.body
-    .then(({ body }) => {
-      console.log(response.bod);
-      const $ = cheerio.load(body); // load method to parse an HTML ==> return Cheerio object
-
-      const results = $('.g ')
-        .map((_, result) => {
-          const $result = $(result);
-          const title = $result.find('.yuRUbf').find('h3').text(); // find specific element
-          const link = $result.find('.yuRUbf').find('a').attr('href');
-          const snippet = $result.find('.VwiC3b').text();
-          const displayedLink = $result.find('.yuRUbf .NJjxre .tjvcx').text();
-          if (link !== undefined) {
-            return {
-              title: title,
-              link: link,
-              snippet: snippet,
-              displayedLink: displayedLink,
-            };
-          }
-        })
-        .toArray();
-
-      res.json(results);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-
-*/
--- userAgent.js
+++ userAgent.js
@@ -1,4 +1,4 @@
-export const selectRandom = () => {
+export const selectRandomUserAgent = () => {
   const userAgents = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36',
@@ -6,7 +6,18 @@ export const selectRandom = () => {
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
+    //
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
   ];
-  let randomNumber = Math.floor(Math.random() * userAgents.length);
-  return userAgents[randomNumber];
+
+  const randomIndex = Math.floor(Math.random() * userAgents.length);
+  const randomUserAgent = userAgents[randomIndex];
+
+  return randomUserAgent;
 };
